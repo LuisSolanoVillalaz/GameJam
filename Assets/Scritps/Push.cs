8169eb0947372c37dbfbd4ef958bdf7a77274caa
Luis Solano <solanoluigi@gmail.com>
2023-02-04 16:36:11 +0100

    Stacked Push

--- Assets/Scritps/Push.cs
+++ Assets/Scritps/Push.cs
@@ -22,6 +22,7 @@ public class Push : MonoBehaviour
 
     }
     void FixedUpdate(){
+        block.velocity = new Vector3(0, block.velocity.y, 0);
          if(isMoving){
                 block.MovePosition( Vector3.MoveTowards(block.position,direc,0.2f));
                 if(block.position==direc){
@@ -29,35 +30,55 @@ public class Push : MonoBehaviour
                 }
                 
             }
+           
     }
+   
     public bool PushTo(float d,Vector3 v){
         bool moved=false;
 
         Vector3 direction=v;
-
+      
 
         //Test for collision in next movement
         Collider[] colobj=Physics.OverlapBox(
             block.position+d*direction, (transform.localScale/2)-
             new Vector3(microdist,microdist,microdist)
         );
+        Collider[] parent=Physics.OverlapBox(
+                block.position+d*Vector3.up, (transform.localScale/2)-
+                new Vector3(microdist,microdist,microdist)
+                );
         if(colobj.Length == 1){
             bool succes=false;
             if(colobj[0].tag=="Box"){
                 Push pushblock =colobj[0].gameObject.GetComponent<Push>();
                 succes=pushblock.PushTo(d,direction);
             }
-            if (succes==true){
+            if (succes==true||colobj[0].tag=="Player"){
+                if(parent.Length == 1){
+                if(parent[0].tag=="Box"){
+                    Push pushblock =parent[0].gameObject.GetComponent<Push>();
+                    pushblock.PushTo(d,direction);
+                }}
                 direc=block.position +(d*direction);
                 isMoving=true;
                 moved=true;
             }
             
         }else if(colobj.Length == 0){
+            if(parent.Length == 1){
+            if(parent[0].tag=="Box"){
+                Push pushblock =parent[0].gameObject.GetComponent<Push>();
+                pushblock.PushTo(d,direction);
+            }}
             direc=block.position +(d*direction);
+            
             isMoving=true;
             moved=true;
         }
+
+        
+       
         return moved;
     }
 }
